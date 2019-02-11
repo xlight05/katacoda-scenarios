bdd043c005795c88c66cf91b1c2923bcd29eef92
Anjana Supun <anjanasupun05@gmail.com>
2019-02-11 06:53:16 +0530

    Updated

--- run-commands-automatically/foreground.sh
+++ run-commands-automatically/foreground.sh
@@ -1,13 +1,13 @@
-./launch.sh
+launch.sh
 echo "Installing ballerina"
 wget https://product-dist.ballerina.io/downloads/0.990.2/ballerina-linux-installer-x64-0.990.2.deb
+chmod +x ballerina-linux-installer-x64-0.990.2.deb
 dpkg -i ballerina-linux-installer-x64-0.990.2.deb
 echo "ballerina installation complete"
 
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cellery-ubuntu-x64-1.0.0.deb
 dpkg - i cellery-ubuntu-x64-1.0.0.deb
 
-sudo apt-get unzip
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cellery-1.0.0-SNAPSHOT.jar
 sudo cp cellery-1.0.0-SNAPSHOT.jar /usr/lib/ballerina/ballerina-0.990.2/bre/lib
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/celleryio.zip
