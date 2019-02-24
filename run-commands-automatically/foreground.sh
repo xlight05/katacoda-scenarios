e186fc3ab9985c9b529d06b324003756e67ff2e9
Anjana Supun <anjanasupun05@gmail.com>
2019-02-24 13:39:36 +0530

    Updated

--- run-commands-automatically/foreground.sh
+++ run-commands-automatically/foreground.sh
@@ -3,7 +3,7 @@ echo "Installing ballerina"
 wget https://product-dist.ballerina.io/downloads/0.990.2/ballerina-linux-installer-x64-0.990.2.deb
 chmod +x ballerina-linux-installer-x64-0.990.2.deb
 dpkg -i ballerina-linux-installer-x64-0.990.2.deb
-echo "ballerina installation complete"
+echo "Installing Cellery"
 
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cellery-ubuntu-x64-1.0.0.deb
 sudo dpkg -i cellery-ubuntu-x64-1.0.0.deb
@@ -15,9 +15,7 @@ wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cell
 unzip celleryio.zip
 sudo cp -r celleryio /usr/lib/ballerina/ballerina-0.990.2/lib/repo/
 
+echo "Installing Cellery runtime"
 git clone https://github.com/xlight05/distribution
 cd distribution/installer/scripts/cellery-runtime-deployer
 cat katakoda.sh | bash -s -- kubeadm
-
-#Cellery Dev
-#Cellery full
