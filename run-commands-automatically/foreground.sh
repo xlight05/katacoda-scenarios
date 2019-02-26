4b88b94b24a2b0386e65cc35d052ad71face1432
Anjana Supun <anjanasupun05@gmail.com>
2019-02-26 14:35:20 +0530

    Updated

--- run-commands-automatically/foreground.sh
+++ run-commands-automatically/foreground.sh
@@ -1,8 +1,8 @@
 launch.sh
 echo "Installing ballerina"
-wget https://product-dist.ballerina.io/downloads/0.990.2/ballerina-linux-installer-x64-0.990.2.deb
-chmod +x ballerina-linux-installer-x64-0.990.2.deb
-dpkg -i ballerina-linux-installer-x64-0.990.2.deb
+wget https://product-dist.ballerina.io/downloads/0.990.3/ballerina-linux-installer-x64-0.990.3.deb
+chmod +x ballerina-linux-installer-x64-0.990.3.deb
+dpkg -i ballerina-linux-installer-x64-0.990.3.deb
 echo "Installing Cellery"
 
 wget https://github.com/cellery-io/sdk/releases/download/v0.1.0/cellery-ubuntu-x64-0.1.0.deb
@@ -10,10 +10,10 @@ sudo dpkg -i cellery-ubuntu-x64-0.1.0.deb
 chmod +x cellery-ubuntu-x64-0.1.0.deb
 
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cellery-0.1.0.jar
-sudo cp cellery-0.1.0.jar /usr/lib/ballerina/ballerina-0.990.2/bre/lib
+sudo cp cellery-0.1.0.jar /usr/lib/ballerina/ballerina-0.990.3/bre/lib
 wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/celleryio.zip
 unzip celleryio.zip
-sudo cp -r celleryio /usr/lib/ballerina/ballerina-0.990.2/lib/repo/
+sudo cp -r celleryio /usr/lib/ballerina/ballerina-0.990.3/lib/repo/
 mkdir -p ~/.ballerina/repo/
 sudo cp  -r celleryio ~/.ballerina/repo/
 
