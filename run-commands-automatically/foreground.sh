e2ac43d91928927a517b7371cdd8e2e90827d855
Anjana Supun <anjanasupun05@gmail.com>
2019-02-27 16:54:15 +0530

    Updated

--- run-commands-automatically/foreground.sh
+++ run-commands-automatically/foreground.sh
@@ -2,16 +2,16 @@ launch.sh
 echo "Installing ballerina"
 wget https://product-dist.ballerina.io/downloads/0.990.3/ballerina-linux-installer-x64-0.990.3.deb
 chmod +x ballerina-linux-installer-x64-0.990.3.deb
-dpkg -i ballerina-linux-installer-x64-0.990.3.deb
-echo "Installing Cellery"
+sudo dpkg -i ballerina-linux-installer-x64-0.990.3.deb
 
+echo "Installing Cellery CLI"
 wget https://github.com/cellery-io/sdk/releases/download/v0.1.0/cellery-ubuntu-x64-0.1.0.deb
 sudo dpkg -i cellery-ubuntu-x64-0.1.0.deb
 chmod +x cellery-ubuntu-x64-0.1.0.deb
 
-wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/cellery-0.1.0.jar
+wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.2/cellery-0.1.0.jar
 sudo cp cellery-0.1.0.jar /usr/lib/ballerina/ballerina-0.990.3/bre/lib
-wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.1/celleryio.zip
+wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.2/celleryio.zip
 unzip celleryio.zip
 sudo cp -r celleryio /usr/lib/ballerina/ballerina-0.990.3/lib/repo/
 mkdir -p ~/.ballerina/repo/
@@ -22,6 +22,5 @@ echo "Installing Cellery runtime"
 git clone https://github.com/xlight05/distribution
 cd distribution/installer/scripts/cellery-runtime-deployer
 git checkout katakoda
+cat cellery-cleanup.sh | bash -s -- kubeadm
 cat katakoda.sh | bash -s -- kubeadm
-echo "New script"
-kubectl apply -f https://gist.githubusercontent.com/xlight05/43fd34f12e161b0998ef349f1de0a947/raw/24d5d8905a550be4be7e058bf340308552ff5328/istio-pilot.yaml
