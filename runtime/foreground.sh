be026c767a5744d8b584697268b8f77f1c89211c
Anjana Supun <anjanasupun05@gmail.com>
2019-03-01 08:11:44 +0530

    Updated

---
+++ runtime/foreground.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Installing ballerina"
+wget https://product-dist.ballerina.io/downloads/0.990.3/ballerina-linux-installer-x64-0.990.3.deb
+chmod +x ballerina-linux-installer-x64-0.990.3.deb
+sudo dpkg -i ballerina-linux-installer-x64-0.990.3.deb
+
+#Installing CLI
+echo "Installing Cellery CLI"
+wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.2/cellery-ubuntu-x64-0.1.0_3.deb
+sudo chmod +x cellery-ubuntu-x64-0.1.0_3.deb
+sudo dpkg -i cellery-ubuntu-x64-0.1.0_3.deb
+sudo chmod -R 777 ~/.ballerina
+
+sudo apt-get install git
+#Installing YAMLS use this only if katacoda supports 
+echo "Installing Cellery runtime"
+git clone https://github.com/xlight05/distribution
+cd distribution/installer/scripts/cellery-runtime-deployer
+git checkout katakoda
+cat katakoda.sh | bash -s -- kubeadm
+
+#Cleanup
+cd ~/
+sudo rm -r distribution
+sudo rm cellery-ubuntu-x64-0.1.0_3.deb
+sudo rm ballerina-linux-installer-x64-0.990.3.deb
+mkdir workspace
+cd workspace
+
+clear
---
+++ runtime/step3.md
@@ -0,0 +1,12 @@
+## Building a Cell
+
+Bulding the cell will generates artifacts thats required to run your Cell.
+This will automatically generate all the kuberentes artifacts for your cell.
+
+`cellery build hello-world.bal -t yourRepo/hello-world-cell:v1.0`{{execute}}
+
+
+## Running a Cell
+
+You can use `run` command to deploy and run your cells in 
+`cellery run hello-world.bal`{{execute}}
