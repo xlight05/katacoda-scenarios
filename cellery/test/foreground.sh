1a0c3ce68a68ee001ac04c423cfffe0a366c4548
Anjana Supun <anjanasupun05@gmail.com>
2019-03-26 15:14:33 +0530

    Updated

--- cellery/test/foreground.sh
+++ cellery/test/foreground.sh
@@ -16,4 +16,16 @@
 #
 # ------------------------------------------------------------------------
 
-sleep 1; wait.sh
+#sleep 1; wait.sh
+
+launch.sh
+wget https://raw.githubusercontent.com/wso2-cellery/distribution/master/installer/packer/minimum-runtime-images/script/cellery.sh
+chmod +x cellery.sh
+./cellery.sh
+rm cellery.sh
+
+wget https://gist.githubusercontent.com/xlight05/3fa261aaef8d32dac4bc4b9d90f0dfd4/raw/89daca1a56721b29efaddece2b954b7c7b5de8be/service-nodeport.yaml
+sed -i 's/172.17.17.100/[[HOST_IP]]/g' service-nodeport.yaml
+kubectl apply -f service-nodeport.yaml
+sudo rm service-nodeport.yaml
+source <(kubectl completion bash)
