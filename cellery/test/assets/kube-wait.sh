85479bc6e9ef2e3a249290da12b5347639616931
Anjana Supun <anjanasupun05@gmail.com>
2019-03-27 13:03:07 +0530

    Updated

---
+++ cellery/test/assets/kube-wait.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+function __is_pod_ready() {
+  [[ "$(kubectl get po "$1" -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]
+}
+
+function __pods_ready() {
+  local pod
+
+  [[ "$#" == 0 ]] && return 0
+
+  for pod in $pods; do
+    __is_pod_ready "$pod" || return 1
+  done
+
+  return 0
+}
+
+function __wait-until-pods-ready() {
+  local period interval i pods
+
+  if [[ $# != 2 ]]; then
+    return 1
+  fi
+
+  period=10000000000
+  interval=5
+
+  for ((i=0; i<$period; i+=$interval)); do
+    pods="$(kubectl get po -n cellery-system -o 'jsonpath={.items[*].metadata.name}')"
+    if __pods_ready $pods; then
+      return 0
+    fi
+    sleep "$interval"
+  done
+
+  return 1
+}
+
+__wait-until-pods-ready $@
+# vim: ft=sh :
--- cellery/test/background.sh
+++ cellery/test/background.sh
@@ -17,34 +17,30 @@
 #
 # ------------------------------------------------------------------------
 
-# launch.sh
-# git clone https://github.com/xlight05/distribution
-# cd distribution
-# git checkout katakoda
-# sed -i 's/wso2-apim/[[HOST_SUBDOMAIN]]-2000-[[KATACODA_HOST]].environments.katacoda.com/g' installer/k8s-artefacts/global-apim/conf/carbon.xml;
-# sed -i 's/WSO2UM_DB/WSO2CarbonDB/g' installer/k8s-artefacts/global-apim/conf/user-mgt.xml;
-# rm installer/k8s-artefacts/global-apim/conf/datasources/master-datasources.xml
-# wget https://raw.githubusercontent.com/xlight05/distribution/katakoda/installer/k8s-artefacts/global-apim/conf/datasources/master-datasources.xml
-# mv master-datasources.xml ~/distribution/installer/k8s-artefacts/global-apim/conf/datasources/
+start=$(date +%s)
+launch.sh
+git clone https://github.com/wso2-cellery/distribution.git
 
+sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/global-idp/conf/carbon.xml
+sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/global-idp/global-idp.yaml
 
-# cd installer/scripts/cellery-runtime-deployer
-# cat katakoda-full.sh | bash -s -- kubeadm
+wget https://gist.githubusercontent.com/xlight05/53b7fa9e23dee876b62f7734ef33c4ee/raw/1942e77839a3421eb54dec87503b6c179c5153a2/katacoda-min.sh
+chmod +x katacoda-min.sh
+./katacoda-min.sh
+rm katacoda-min.sh
 
-# #Cleanup
-# cd ~/
-# sudo rm -r distribution
-# cd tutorial
+wget https://gist.githubusercontent.com/xlight05/3fa261aaef8d32dac4bc4b9d90f0dfd4/raw/89daca1a56721b29efaddece2b954b7c7b5de8be/service-nodeport.yaml
+sed -i 's/172.17.17.100/[[HOST_IP]]/g' service-nodeport.yaml
+kubectl apply -f service-nodeport.yaml
+sudo rm service-nodeport.yaml
+source <(kubectl completion bash)
 
-# wget https://gist.githubusercontent.com/xlight05/3fa261aaef8d32dac4bc4b9d90f0dfd4/raw/89daca1a56721b29efaddece2b954b7c7b5de8be/service-nodeport.yaml
-# sed -i 's/172.17.17.100/[[HOST_IP]]/g' service-nodeport.yaml
-# wget https://gist.githubusercontent.com/xlight05/73f50180840c40d25f9c9c7865054090/raw/99ddfe2869d055b271da07e21fb6d7f6f964b646/ingress.yaml
-# sed -i 's/wso2-apim-gateway/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' ingress.yaml;
-# sed -i 's/wso2-apim/[[HOST_SUBDOMAIN]]-2000-[[KATACODA_HOST]].environments.katacoda.com/g' ingress.yaml;
-# kubectl apply -f service-nodeport.yaml
-# kubectl apply -f ingress.yaml -n cellery-system
-# sudo rm service-nodeport.yaml
-# sudo rm ingress.yaml
+wget https://raw.githubusercontent.com/wso2-cellery/mesh-controller/master/samples/pet-store-yamls/pet-backend.yaml
+wget https://raw.githubusercontent.com/wso2-cellery/mesh-controller/af77d802c3bb4be87094db0ba98a7c8ea66de160/samples/pet-store-yamls/pet-frontend.yaml
+sed -i 's/pet-store.com/[[HOST_SUBDOMAIN]]-2000-[[KATACODA_HOST]].environments.katacoda.com/g' pet-frontend.yaml;
+sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' pet-frontend.yaml;
 
-# source <(kubectl completion bash)
-# echo "done" >> /root/katacoda-finished
+kube-wait.sh
+echo "done" >> /root/katacoda-finished
+end=$(date +%s)
+echo "Took $(($end-$start)) seconds"
