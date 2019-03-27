a4853c7b103f322ad954c53dca597ba65171f1bb
Anjana Supun <anjanasupun05@gmail.com>
2019-03-27 15:33:39 +0530

    Updated

--- cellery/test/assets/kube-wait.sh
+++ cellery/test/assets/kube-wait.sh
@@ -3,36 +3,33 @@
 set -e
 
 function __is_pod_ready() {
-  [[ "$(kubectl get po "$1" -n cellery-system -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]
+    [[ "$(kubectl get po "$1" -n cellery-system -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]
 }
 
 function __pods_ready() {
-  local pod
-
-  [[ "$#" == 0 ]] && return 0
-
-  for pod in $pods; do
-    __is_pod_ready "$pod" || return 1
-  done
-
-  return 0
+    local pod
+    
+    [[ "$#" == 0 ]] && return 0
+    
+    for pod in $pods; do
+        __is_pod_ready "$pod" || return 1
+    done
+    
+    return 0
 }
 
 function __wait-until-pods-ready() {
-  local period interval i pods
-
-  period=10000000000
-  interval=5
-
-  for ((i=0; i<$period; i+=$interval)); do
-    pods="$(kubectl get po -n cellery-system -o 'jsonpath={.items[*].metadata.name}')"
-    if __pods_ready $pods; then
-      return 0
-    fi
-    sleep "$interval"
-  done
-
-  return 1
+    interval=3
+    local period interval i pods
+    while :
+    do
+        pods="$(kubectl get po -n cellery-system -o 'jsonpath={.items[*].metadata.name}')"
+        if __pods_ready $pods; then
+            return 0
+        fi
+        sleep "$interval"
+    done
+    return 1
 }
 
 __wait-until-pods-ready
