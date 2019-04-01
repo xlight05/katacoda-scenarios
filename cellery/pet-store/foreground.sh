184587ce7b9bcc2fc4998779cc1b5556f1901435
Anjana Supun <anjanasupun05@gmail.com>
2019-04-01 09:51:32 +0530

    Updated

--- cellery/pet-store/foreground.sh
+++ cellery/pet-store/foreground.sh
@@ -17,55 +17,5 @@
 # ------------------------------------------------------------------------
 export ORG_NAME="wso2-cellery"
 export CELLERY_HOME="/usr/bin/cellery"
-#sleep 2; wait.sh
-start=$(date +%s)
-launch.sh
+sleep 2; wait.sh
 
-git clone https://github.com/wso2-cellery/distribution.git
-git clone https://github.com/wso2-cellery/mesh-observability
-
-sudo apt-get remove -y cellery
-wget https://github.com/xlight05/katacoda-scenarios/releases/download/0.0.2/cellery-ubuntu-x64-0.1.1.deb
-sudo dpkg -i cellery-ubuntu-x64-0.1.1.deb
-sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/global-idp/conf/carbon.xml
-sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/global-idp/global-idp.yaml
-
-#cellery-dashboard
-sed -i 's/cellery-dashboard/[[HOST_SUBDOMAIN]]-4000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/observability/portal/observability-portal.yaml
-#TODO portal.json
-
-#cellery-k8s-metrics
-sed -i 's/cellery-k8s-metrics/[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/observability/grafana/config/grafana.ini
-sed -i 's/cellery-k8s-metrics/[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/observability/grafana/k8s-metrics-grafana.yaml
-sed -i 's/cellery-k8s-metrics/[[HOST_SUBDOMAIN]]-5000-[[KATACODA_HOST]].environments.katacoda.com/g' mesh-observability/components/global/portal/io.cellery.observability.ui/node-server/config/portal.json
-
-#wso2sp-observability-api
-sed -i 's/wso2sp-observability-api/[[HOST_SUBDOMAIN]]-6000-[[KATACODA_HOST]].environments.katacoda.com/g' mesh-observability/components/global/portal/io.cellery.observability.ui/node-server/config/portal.json
-sed -i 's/wso2sp-observability-api/[[HOST_SUBDOMAIN]]-6000-[[KATACODA_HOST]].environments.katacoda.com/g' distribution/installer/k8s-artefacts/observability/sp/sp-worker.yaml
-
-mid=$(date +%s)
-echo "Took $(($mid-$start)) seconds "
-
-wget https://gist.githubusercontent.com/xlight05/47f325fd883f97c9d92cb972930deafc/raw/5b98b6ac15d599a5b67b15e526e0a2a6ac9f5ed0/katacoda-minobs.sh
-chmod +x katacoda-minobs.sh
-./katacoda-minobs.sh
-rm katacoda-minobs.sh
-
-wget https://gist.githubusercontent.com/xlight05/3fa261aaef8d32dac4bc4b9d90f0dfd4/raw/89daca1a56721b29efaddece2b954b7c7b5de8be/service-nodeport.yaml
-sed -i 's/172.17.17.100/[[HOST_IP]]/g' service-nodeport.yaml
-kubectl apply -f service-nodeport.yaml
-sudo rm service-nodeport.yaml
-source <(kubectl completion bash)
-
-wget https://raw.githubusercontent.com/wso2-cellery/mesh-controller/master/samples/pet-store-yamls/pet-backend.yaml
-wget https://raw.githubusercontent.com/wso2-cellery/mesh-controller/af77d802c3bb4be87094db0ba98a7c8ea66de160/samples/pet-store-yamls/pet-frontend.yaml
-sed -i 's/pet-store.com/[[HOST_SUBDOMAIN]]-2000-[[KATACODA_HOST]].environments.katacoda.com/g' pet-frontend.yaml;
-sed -i 's/idp.cellery-system/[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/g' pet-frontend.yaml;
-
-done=$(date +%s)
-echo "Took $(($done-$mid)) to finish script "
-
-kube-wait.sh
-echo "done" >> /root/katacoda-finished
-end=$(date +%s)
-echo "Took $(($end-$start)) seconds "
