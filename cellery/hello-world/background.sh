9109ccf4496ed53cf98c97bbed76d14155fc2ea5
Anjana Supun <anjanasupun05@gmail.com>
2019-04-11 10:37:56 +0530

    Updated

--- cellery/hello-world/background.sh
+++ cellery/hello-world/background.sh
@@ -18,8 +18,6 @@
 # ------------------------------------------------------------------------
 launch.sh
 
-git clone https://github.com/wso2-cellery/distribution.git
-
 sudo apt-get remove -y cellery
 wget https://wso2.org/jenkins/job/cellery/job/sdk/124/artifact/installers/ubuntu-x64/target/cellery-ubuntu-x64-40772520b7f999ce62136c2417e138dfef8d1fab.deb
 sudo dpkg -i cellery-ubuntu-x64-40772520b7f999ce62136c2417e138dfef8d1fab.deb
