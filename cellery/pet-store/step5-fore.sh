b282978845cdcf4eb176e1c7e5e8f5a83285d1d3
Anjana Supun <anjanasupun05@gmail.com>
2019-04-23 12:38:46 +0530

    Updated

--- cellery/pet-store/step5-fore.sh
+++ cellery/pet-store/step5-fore.sh
@@ -17,27 +17,4 @@
 #
 # ------------------------------------------------------------------------
 
-show_progress()
-{
-  local -r pid="${1}"
-  local -r delay='0.75'
-  local spinstr='\|/-'
-  local temp
-  while true; do
-    sudo grep -i "done" /root/obs-finished &> /dev/null
-    if [[ "$?" -ne 0 ]]; then
-      echo -n "Waiting for the observability portal"
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-}
-
-show_progress
+wait-obs.sh
